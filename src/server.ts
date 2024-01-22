40601f30ae9f512965bc05029b47627c6257eb44
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-22 10:44:01 +0900

    chore: setup dev plugin

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import type { ViteDevServer, Connect } from 'vite'
+import fs from "node:fs";
+import { render } from "./entry-server";
+
+// exposed by globalThis.__viteDevServer in devPlugin
+declare let __viteDevServer: ViteDevServer;
+
+const handler: Connect.NextHandleFunction = async (_req, res) => {
+  // template
+  let html = await fs.promises.readFile('./index.html', 'utf-8');
+  html = await __viteDevServer.transformIndexHtml("/", html);
+
+  // ssr
+  const ssrResult = render();
+  html = html.replace("<!--app-html-->", ssrResult.html)
+  
+  res.setHeader("content-type", "text/html").end(html);
+}
+
+export default handler;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type Plugin, type Connect } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  clearScreen: false,
+  plugins: [
+    react(),
+    devPlugin({
+      entry: "/src/server.ts",
+      useViteRuntime: false,
+    })
+  ]
 })
+
+// vavite style dev server with ViteRuntime.executeEntrypoint
+// https://github.com/hi-ogawa/vite-plugins/pull/156
+function devPlugin({
+  entry,
+  useViteRuntime,
+}: {
+  entry: string
+  useViteRuntime?: boolean;
+}): Plugin {
+  return {
+    name: devPlugin.name,
+
+    apply: "serve",
+
+    async configureServer(server) {
+      // expose viteDevServer
+      globalThis.__viteDevServer = server;
+
+      // switch module loader
+      let loadModule = server.ssrLoadModule;
+      if (useViteRuntime) {
+        const vite: any = await import("vite");
+        const runtime = await vite.createViteRuntime(server);
+        loadModule = runtime.executeEntrypoint;
+      }
+
+      const handler: Connect.NextHandleFunction = async (req, res, next) => {
+        try {
+          const mod = await loadModule(entry);
+          await mod["default"](req, res, next);
+        } catch (e) {
+          next(e);
+        }
+      };
+      return () => server.middlewares.use(handler);
+    }
+  }
+}
