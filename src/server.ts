ca4474eeaf89d3f7f5cce2b81f888f4a46e051a9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-15 14:47:38 +0900

    chore: fix server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
-import type { Connect } from "vite";
+import type { ViteDevServer, Connect } from 'vite'
 import fs from "node:fs";
 import { render } from "./entry-server";
 
-const handler: Connect.NextHandleFunction = async (req, res) => {
+// exposed by globalThis.__viteDevServer in devPlugin
+declare let __viteDevServer: ViteDevServer;
+
+const handler: Connect.NextHandleFunction = async (_req, res) => {
   // template
-  let html = await fs.promises.readFile("./index.html", "utf-8");
-  html = await (req as any).viteDevServer.transformIndexHtml("/", html);
+  let html = await fs.promises.readFile('./index.html', 'utf-8');
+  html = await __viteDevServer.transformIndexHtml("/", html);
 
   // ssr
   const ssrResult = render();
-  html = html.replace("<!--app-html-->", ssrResult.html);
-
+  html = html.replace("<!--app-html-->", ssrResult.html)
+  
   res.setHeader("content-type", "text/html").end(html);
-};
+}
 
 export default handler;
