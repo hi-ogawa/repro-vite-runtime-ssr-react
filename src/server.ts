6610e5fa9af1be591f7b2b7b7f17ffac119948ad
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-15 14:49:39 +0900

    chore: lint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import type { ViteDevServer, Connect } from 'vite'
+import type { ViteDevServer, Connect } from "vite";
 import fs from "node:fs";
 import { render } from "./entry-server";
 
@@ -7,14 +7,14 @@ declare let __viteDevServer: ViteDevServer;
 
 const handler: Connect.NextHandleFunction = async (_req, res) => {
   // template
-  let html = await fs.promises.readFile('./index.html', 'utf-8');
+  let html = await fs.promises.readFile("./index.html", "utf-8");
   html = await __viteDevServer.transformIndexHtml("/", html);
 
   // ssr
   const ssrResult = render();
-  html = html.replace("<!--app-html-->", ssrResult.html)
-  
+  html = html.replace("<!--app-html-->", ssrResult.html);
+
   res.setHeader("content-type", "text/html").end(html);
-}
+};
 
 export default handler;
